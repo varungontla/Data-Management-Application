8c29c79ca303fd4bbd93b7f8dcafaa02e6b1f3e9
Gontla Venkata Subba Varun <varungontla02@gmail.com>
2022-11-14 13:34:23 +0530

    Django Update

--- CNproject/settings.py
+++ CNproject/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 
-EMAIL_HOST_PASSWORD = 
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
